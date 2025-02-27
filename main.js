98ca19237a572e9cd73e36aa397c430ee34b992f
NgHongHa <ha2k3.dev@gmail.com>
2025-02-27 17:04:04 +0700

    Các hàm toán học

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
-// // các phép so sánh
-// != khác
-// > lớn hơn
-// < nhỏ hơn
-// >= lớn hơn hoặc bằng
-// <= nhỏ hơn hoặc bằng
-// == so sánh bằng ko so sánh kiểu dữ liệu
-// ==== so sánh bằng có so sánh kiểu dữ liệu
-// tương tự !==
+// Các hàm toán học
+// 1 hàm căn bậc 2 Math.sqrt
+let a = Math.sqrt(4);
+// 2 hàm mũ Math.pow
+let b = Math.pow(2, 3);
+// 3 hàm trị tuyệt đối abs
+let c = Math.abs(-5);
+// 4làm tròn lên và làm tròn xuống
+let d = Math.ceil(4.02);
+// let e = Math.floor(5.005);
+// 5 làm tròn 0.5 thành 1
+let e = Math.round(7.6666666);
+// 6 làm tròn số x chữ số phần đơn vị ( lưu ý kết quả trả về string)
+let f = 3.4432523535;
+// let fLamtron = f.toFixed(2);
+let fLamtron = Number(f.toFixed(2)); //recommend
