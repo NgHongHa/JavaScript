51def3a576852919f2e0f704c450716e2f2438b6
NgHongHa <ha2k3.dev@gmail.com>
2025-02-27 15:54:07 +0700

    thứ tự ưu tiên

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-// các phép toán cơ bản
-let a = 5;
-let b = 2;
-console.log((tong = a + b));
-console.log((hieu = a - b));
-console.log((tich = a * b));
-console.log((thuong = a / b));
-console.log((chialaydu = a % b));
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_precedence
+// thứ tự ưu tiên
+let diemToan = 9.5;
+let diemVan = 8.75;
+let diemTb = (diemToan + diemVan) / 2; // tạo () bôi đen shift + 9
+// phải qua trái
+let x = (y = 25);
+// trái qua phải
+let c = 25 - 5 - 8 + 9;
