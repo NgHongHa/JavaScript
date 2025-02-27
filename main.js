9612e86e51949d74af61bfd48f7d1c0eef627950
NgHongHa <ha2k3.dev@gmail.com>
2025-02-27 15:42:09 +0700

    ác cách xuất dữ liệu trong js

--- main.js
+++ main.js
@@ -1,22 +1,12 @@
-// các kiểu dữ liệu trong js
-// 1 string
-let myString = "đây là chuỗi";
-console.log(myString);
-console.log(typeof myString);
-// number kiểu số (số ngyên hoặc số thực)
-let soNguyen = 10;
-let soThuc = 3.14;
-console.log(soNguyen);
-console.log(soThuc);
-// kiểu boolen
-let check = true;
-console.log(typeof check);
-// kiểu undefined
-let diem;
-console.log(typeof diem);
-// kiểu null
-let bug = null;
-console.log(typeof bug); // lỗi này sẽ cho kiểu dữ liệu của null là object
-// xuất số nguyên an toàn tối đa và tối thiểu
-console.log(" số nguyên an toàn tối đa" + Number.MAX_SAFE_INTEGER);
-console.log(" số nguyên an toàn tối thiểu" + Number.MIN_SAFE_INTEGER);
+// các cách xuất dữ liệu trong js
+let soA = 25;
+let soB = 5;
+// cách 1
+console.log("căn bậc 2 của " + soA + " là " + soB);
+// cách 2
+console.log("căn bậc 2 của ", soA, "là", soB);
+// cách 3
+console.log(`căn bậc 2 của ${soA} là ${soB}`);
+// cách 4
+console.log(`căn bậc 2 của %s là %s`, soA, soB);
+// ctrl +D để bôi đen hàng loạt
