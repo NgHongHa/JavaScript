239fd1260962b34c325c71cf9b9df5fc0aca8fb9
NgHongHa <ha2k3.dev@gmail.com>
2025-02-27 15:07:24 +0700

    alert and console.log

---
+++ main.js
@@ -0,0 +1,5 @@
+// Các hàm được built sẵn trong js
+// Hàm aler hiển thị thông báo dạng popup
+alert("xin chào");
+// hàm consolelog không hiển thị trong trình duyệt, chỉ hiển thị ở tab console thường dùng để debug
+console.log("xin chào");
