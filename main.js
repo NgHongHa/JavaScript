9869d22584e45bd35f3f6fb717077a847d0461c9
NgHongHa <ha2k3.dev@gmail.com>
2025-02-27 16:05:59 +0700

    ép kiểu

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_precedence
-// thứ tự ưu tiên
-let diemToan = 9.5;
-let diemVan = 8.75;
-let diemTb = (diemToan + diemVan) / 2; // tạo () bôi đen shift + 9
-// phải qua trái
-let x = (y = 25);
-// trái qua phải
-let c = 25 - 5 - 8 + 9;
+// ép kiểu đữ liệu
+let numberA = prompt("nhập vào số A :");
+console.log(typeof numberA); // lúc này  số A kiểu string nên cần ép kiểu về number để tính toán
+let numberB = 5;
+// cộng a với b (nếu là các phép tính khác thì tính bình thường)
+let kq = numberA + numberB;
+console.log(kq); // saiii
+// ép kiểu để tính toán
+let numberC = parseInt(prompt("nhập vào số c :"));
+console.log(typeof numberC);
+//  cách ép kiểu bao gồm parseInt , parseFloat Number
