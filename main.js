090d3188a3c324d0011fc6a49ff7eff081e12c85
NgHongHa <ha2k3.dev@gmail.com>
2025-02-27 16:35:56 +0700

    // // toán tử logic

--- main.js
+++ main.js
@@ -1,30 +1,6 @@
-// toán tử gán
-let x1 = 10;
-let y1 = 5;
-let z1 = x1 + y1;
-console.log(x1);
-console.log(y1);
-console.log(z1);
-// gán += -= *= /= %=
-let m = 10;
-m += 5;
-console.log(m);
-m -= 5;
-console.log(m);
-m *= 5;
-console.log(m);
-m /= 5;
-console.log(m);
-m %= 5;
-console.log(m);
-// toán tử ++ -- tiền tố hậu tố (tính tiền tố trước)
-let x = 1;
-let y = 2;
-let z = x++ - ++y + 1;
-// let z = x++ - ++y + 1;
-//  z = ( x++ = 1) - (y ++ = 3) + 1 = -1
-//  y = 3
-//  x = 2
-console.log(x);
-console.log(y);
-console.log(z);
+// // toán tử logic
+// && tre khi cả 2 đều đúng
+let i = 7;
+console.log(i > 0 && i < 10);
+// || toán tử hoặc
+// !toán tử phủ định
