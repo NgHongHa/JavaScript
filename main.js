724cce1c7715ac2c12703a0e03bae9c1c90db2c5
NgHongHa <ha2k3.dev@gmail.com>
2025-02-27 15:47:21 +0700

    các phép toán cơ bản

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
-// các cách xuất dữ liệu trong js
-let soA = 25;
-let soB = 5;
-// cách 1
-console.log("căn bậc 2 của " + soA + " là " + soB);
-// cách 2
-console.log("căn bậc 2 của ", soA, "là", soB);
-// cách 3
-console.log(`căn bậc 2 của ${soA} là ${soB}`);
-// cách 4
-console.log(`căn bậc 2 của %s là %s`, soA, soB);
-// ctrl +D để bôi đen hàng loạt
+// các phép toán cơ bản
+let a = 5;
+let b = 2;
+console.log((tong = a + b));
+console.log((hieu = a - b));
+console.log((tich = a * b));
+console.log((thuong = a / b));
+console.log((chialaydu = a % b));
