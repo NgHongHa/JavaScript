e75d7f35436a755855b865861e1814eb0faaa887
NgHongHa <ha2k3.dev@gmail.com>
2025-02-27 16:29:14 +0700

    toán tử gán, tiền tố + hậu tố

--- main.js
+++ main.js
@@ -1,11 +1,30 @@
-// ép kiểu đữ liệu
-let numberA = prompt("nhập vào số A :");
-console.log(typeof numberA); // lúc này  số A kiểu string nên cần ép kiểu về number để tính toán
-let numberB = 5;
-// cộng a với b (nếu là các phép tính khác thì tính bình thường)
-let kq = numberA + numberB;
-console.log(kq); // saiii
-// ép kiểu để tính toán
-let numberC = parseInt(prompt("nhập vào số c :"));
-console.log(typeof numberC);
-//  cách ép kiểu bao gồm parseInt , parseFloat Number
+// toán tử gán
+let x1 = 10;
+let y1 = 5;
+let z1 = x1 + y1;
+console.log(x1);
+console.log(y1);
+console.log(z1);
+// gán += -= *= /= %=
+let m = 10;
+m += 5;
+console.log(m);
+m -= 5;
+console.log(m);
+m *= 5;
+console.log(m);
+m /= 5;
+console.log(m);
+m %= 5;
+console.log(m);
+// toán tử ++ -- tiền tố hậu tố (tính tiền tố trước)
+let x = 1;
+let y = 2;
+let z = x++ - ++y + 1;
+// let z = x++ - ++y + 1;
+//  z = ( x++ = 1) - (y ++ = 3) + 1 = -1
+//  y = 3
+//  x = 2
+console.log(x);
+console.log(y);
+console.log(z);
