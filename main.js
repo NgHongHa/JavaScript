ef1be77a19b611c0c35b329352cf664d3f44d43d
NgHongHa <ha2k3.dev@gmail.com>
2025-02-28 13:22:05 +0700

    for

--- main.js
+++ main.js
@@ -1,15 +1,6 @@
-// do while
-// chạy ít nhất 1 lần đầu tiên
-let i = 0;
-do {
-  console.log(i);
-  i++;
-} while (i <= 5);
-// while true
-while (true) {
-  i++;
-  console.log(i); // vòng lặp này luôn đúng =>> vô tận
-  if (i >= 10) {
-    break;
-  }
+// for
+let tong = 0;
+for (let i = 0; i < 5; i++) {
+  tong += i;
 }
+console.log(tong);
