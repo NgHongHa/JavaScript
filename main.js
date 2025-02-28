59fcc5994b8e6da6b2fb94f572b7bf7ac9550ab0
NgHongHa <ha2k3.dev@gmail.com>
2025-02-28 13:16:50 +0700

    do while and while true

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
-// // vòng lặp while
-// // thực hiện đến khi điều kiện không còn đúng sẽ thoát vòng lặp
-// let i = 1;
-// while (i <= 5) {
-//   console.log(i);
-//   i++;
-// }
-// ví dụ nhập n trong khoảng từ 1 đến 99 nếu nhập sai thì bắt nhập lại
-let n = prompt(" nhập vào số  n  (từ 1 đến 99)");
-console.log(n);
-while (isNaN(n) || n < 1 || n > 99 || n % 1 !== 0) {
-  n = Number(prompt("số bạn nhập ko hợp lệ vui lòng nhập lại"));
+// do while
+// chạy ít nhất 1 lần đầu tiên
+let i = 0;
+do {
+  console.log(i);
+  i++;
+} while (i <= 5);
+// while true
+while (true) {
+  i++;
+  console.log(i); // vòng lặp này luôn đúng =>> vô tận
+  if (i >= 10) {
+    break;
+  }
 }
-alert("bạn đã nhập số : " + n);
