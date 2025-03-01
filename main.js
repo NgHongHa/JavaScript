f39cfade4b507e0862ba980dbabe05f60505f2d4
NgHongHa <ha2k3.dev@gmail.com>
2025-03-01 11:40:08 +0700

    slice && substring

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
-// đệ quy
-function giaiThua(N) {
-  if (N === 0 || N === 1) {
-    return 1;
-  } else {
-    return N * giaiThua(N - 1);
-  }
-}
-var kq = giaiThua(3);
-console.log(kq);
-// dãy fibonaci  1 1 2 3 5 8 13 21 ---- fn = f(n-1)+ f(n-2) điểm dừng n <=2 thì fn =1
-function f(n) {
-  if (n <= 2) {
-    return 1;
-  } else {
-    return f(n - 1) + f(n - 2);
-  }
-}
-// gọi hàm
-kqf = f(10);
-console.log(kqf);
+// kiểu chuỗi
+// 1 các cách xuất chuỗii
+// có 3 cách chuỗi '' "" ``
+// quy tắc index trong chuỗi
+let ten = "John";
+console.log(ten[1]);
+// kiểm tra chiều dài của chuỗi
+console.log(ten.length);
+// cắt chuỗi slice
+let fullName = "Nguyen Hong Ha";
+let fistName = fullName.slice(7, 11); // 11 là end index nếu ko nhập sẽ ngầm cắt đến cuối chuỗi
+console.log(fistName);
+//substring
+let name = fullName.substring(12, 14);
+console.log(name);
+/* so sánh slice và substring
+nếu nhập chỉ số đầu lớn hơn chỉ số sau
+let fistName = fullName.slice(11, 7) trả về chuỗi rỗng ""
+let name = fullName.substring(11, 7); trả về kết quả như substring(7, 11)
+nếu nhập tham số âm
+ - substring sẽ tự hiểu số âm thành số 0
+ - slice nếu truyền số âm thì tính từ cuối chuỗi ( ví dụ -1 là kí tự cuối cùng của chuỗi)
