7c8c0d35f2303ab9d3b50b3ed862e125f1642582
NgHongHa <ha2k3.dev@gmail.com>
2025-03-01 11:45:43 +0700

    trim, trimEnd, trimStart

--- main.js
+++ main.js
@@ -1,22 +1,9 @@
-// kiểu chuỗi
-// 1 các cách xuất chuỗii
-// có 3 cách chuỗi '' "" ``
-// quy tắc index trong chuỗi
-let ten = "John";
-console.log(ten[1]);
-// kiểm tra chiều dài của chuỗi
-console.log(ten.length);
-// cắt chuỗi slice
-let fullName = "Nguyen Hong Ha";
-let fistName = fullName.slice(7, 11); // 11 là end index nếu ko nhập sẽ ngầm cắt đến cuối chuỗi
-console.log(fistName);
-//substring
-let name = fullName.substring(12, 14);
-console.log(name);
-/* so sánh slice và substring
-nếu nhập chỉ số đầu lớn hơn chỉ số sau
-let fistName = fullName.slice(11, 7) trả về chuỗi rỗng ""
-let name = fullName.substring(11, 7); trả về kết quả như substring(7, 11)
-nếu nhập tham số âm
- - substring sẽ tự hiểu số âm thành số 0
- - slice nếu truyền số âm thì tính từ cuối chuỗi ( ví dụ -1 là kí tự cuối cùng của chuỗi)
+// xóa khoảng trắng ở chuỗi
+let s1 = "     hello     ";
+console.log(s1.length);
+let s2 = s1.trim(); // cả trước và sau
+console.log(s2.length);
+let s3 = s1.trimEnd(); //sau
+console.log(s3);
+let s4 = s1.trimStart(); //trước
+console.log(s4);
