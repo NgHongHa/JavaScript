6ac8ad8e103c6e91f07bb3b4824a55c60fc30d67
NgHongHa <ha2k3.dev@gmail.com>
2025-03-03 18:15:04 +0700

    .filter()

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
-const numbers = [1, 2, 3, 4, 5];
-
-const sum = numbers.reduce(
-  (accumulator, currentValue) => accumulator + currentValue,
-  0
-);
-
-console.log(sum); // Output: 15
-/*
-accumulator: Biến tích lũy giá trị trong mỗi lần lặp.
-currentValue: Giá trị hiện tại của phần tử trong mảng.
-0: Giá trị khởi tạo cho accumulator.
+let number = [1, 2, 3, 4, 5];
+let evennumber = number.filter((vale, index, array) => vale % 2 == 0);
+console.log(evennumber);
+/*.filter() là một phương thức của mảng trong JavaScript, giúp lọc ra các phần tử thỏa mãn điều kiện.
+Hàm callback (value, index, array) => value % 2 == 0 được thực thi cho từng phần tử trong mảng:
+value: giá trị của phần tử hiện tại.
+index: chỉ mục (vị trí) của phần tử trong mảng.
+array: chính mảng number gốc.
+Điều kiện value % 2 == 0 kiểm tra xem phần tử có chia hết cho 2 hay không (tức là số chẵn).
+*/
