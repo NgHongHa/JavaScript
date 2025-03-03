8c7280472e64650649f6a53598d61e52c0e5d4d0
NgHongHa <ha2k3.dev@gmail.com>
2025-03-03 16:16:51 +0700

    array js

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
-// setTimeout
-let hello = function () {
-  console.log("Helllo");
-};
-setTimeout(hello, 3000);
-setTimeout(() => {
-  console.log("hello2");
-}, 5000);
-// setInterval  lặp đi lặp lại sau time cố định
+// mảng array js
+let array = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log(array);
+console.log(array.length); // kiểm tra độ dài
+array[3] = 10;
+console.log(array);
+// duyệt mảng for
+for (let i = 0; i < array.length; i++) {
+  console.log(array[i] + " Hà");
+}
+// dùng for off ( chỉ xem)
+for (let views of array) {
+  console.log(views);
+}
