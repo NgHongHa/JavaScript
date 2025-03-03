9c263d4d5c3edd8520f9259ea1231161f079f3e0
NgHongHa <ha2k3.dev@gmail.com>
2025-03-03 22:42:26 +0700

    một số phương thức có sẵn trong oop

--- main.js
+++ main.js
@@ -1,59 +1,26 @@
-// #####34.1 - oop buoi 1 - tổng quan - Literal syntax #####
-// Ôn tập kiến thức cũ
-let a = 5;
-let b = 7.5;
-console.log(typeof a);
-console.log(typeof b);
-let check = true;
-console.log(typeof check);
-
-let str = "Hôm nay tôi học obj";
-console.log(typeof str);
-
-M = ["hoa", "trang", "lan", "mai"];
-console.log(typeof M);
-
-// 2. khai báo đối tượng , literal syntax
-let student = {
-  // key + value
-  // thuộc tính
-  fullName: "Trần Như Nhộng",
-  birthYear: 2005,
-  address: {
-    city: "Hà Nội",
-    country: "Việt Nam",
-  },
-  scores: [8, 9, 10],
-  // phương thức
-  getAge: function () {
-    return 2024 - this.birthYear;
-  },
-
-  diemTrungBinh: function () {
-    // tính tổng điểm
-    sumScores = this.scores.reduce((a, b) => a + b, 0);
-    // Trả về dtb
-    return sumScores / 3;
-  },
+// một số phương thức có sẵn
+let person = {
+  name: "Ha",
+  age: 22,
 };
-
-let teacher = {
-  "1 fullName": "Jacob",
-  "@address": {
-    city: "Hanoi",
-    country: "VietNam",
-  },
+let keys = Object.keys(person); // trả về key
+let keys2 = Object.values(person); // trả về value
+let entries = Object.entries(person); // trả về cả key và value
+// assin sao chép src 1, 2 vàp target
+let target = { c: 100 };
+let src1 = { a: 0 };
+let src2 = { b: 50 };
+Object.assign(target, src1, src2);
+console.log(target);
+// kiểm tra có thuộc tính không
+let person2 = {
+  name: "Ha",
+  age: 22,
 };
-2; // truy cập thuộc tính
-console.log(student.fullName);
-console.log(teacher["1 fullName"]); // dùng khi gặp các key đặc biệt ( có khoảng trắng)
-// 3  truy cập phương thức
-console.log(student.getAge()); // lưu ý khi dùng phương thức phải thêm ()
-// 4 thêm, xóa thuộc tính
-student.email = "ha2k3.dev@gamil.com";
-student["web"] = "http///";
-// thêm thuộc tính mới
-console.log(student);
-//_______________________
-delete student.email; // xóa
-student.web = "http"; // sửa
+console.log(person2.hasOwnProperty("name")); // kiểm tra trong person2 có thuộc tính name không
+Object.freeze(person2); // đóng băng đối tượng không thể thêm  sửa xóa
+let person3 = {
+  name: "Ha",
+  age: 12,
+};
+Object.seal(person3); // chỉ sửa không thêm xóa được
