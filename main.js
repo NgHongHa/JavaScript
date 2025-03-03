8de8fd6cead8c0c0dc885694ed6d17ec5dbf21ac
NgHongHa <ha2k3.dev@gmail.com>
2025-03-03 22:08:00 +0700

    Object

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-let number = [1, 2, 3, 4, 5];
-let evennumber = number.filter((vale, index, array) => vale % 2 == 0);
-console.log(evennumber);
-/*.filter() là một phương thức của mảng trong JavaScript, giúp lọc ra các phần tử thỏa mãn điều kiện.
-Hàm callback (value, index, array) => value % 2 == 0 được thực thi cho từng phần tử trong mảng:
-value: giá trị của phần tử hiện tại.
-index: chỉ mục (vị trí) của phần tử trong mảng.
-array: chính mảng number gốc.
-Điều kiện value % 2 == 0 kiểm tra xem phần tử có chia hết cho 2 hay không (tức là số chẵn).
-*/
+// Object
+let student = {
+  fullName: "Nguyen Hong Ha",
+  birthYear: 2003,
+  address: {
+    city: "HN",
+    country: "vn",
+  },
+  getAge: function () {
+    return 2024 - this.birthYear;
+  },
+};
