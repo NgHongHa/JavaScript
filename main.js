4131e4737f8dc528eef01726f401526c41e94991
NgHongHa <ha2k3.dev@gmail.com>
2025-03-03 15:40:59 +0700

    split

--- main.js
+++ main.js
@@ -1,12 +1,4 @@
-let s1 = "học học nữa học mãi";
-console.log(s1.startsWith("h"));
-//startsWith kiểm tra chuỗi có bắt đầu bằng h không
-
-console.log(s1.startsWith("ọ", 1));
-//startsWith kiểm tra chuỗi tính từ index1 có bắt đầu bằng h không
-
-console.log(s1.endsWith("i"));
-//endsWithWith kiểm tra chuỗi có kết thúc bằng h không
-
-console.log(s1.endsWith("a", 11));
-//startsWith kiểm tra chuỗi tính từ đầu đến 10 có kt là a không ( kí tự index 10 có phải a không) ++ nó chỉ kiểm tra đến sát 11 khi nhập vào 11 chưa đến 11
+// tách chuỗi trả về mảng bằng split
+let students = "Hà, Nam , Dũng, Khoa";
+let arr = students.split(","); // nếu nhập vào chuỗi rỗng sẽ tác từng kí tự
+console.log(arr);
