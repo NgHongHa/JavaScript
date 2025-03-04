46fd4349a208057ca6aa2bd0072d7d364c5c808a
NgHongHa <ha2k3.dev@gmail.com>
2025-03-04 09:54:41 +0700

    Function constructor

--- main.js
+++ main.js
@@ -1,26 +1,14 @@
-// một số phương thức có sẵn
-let person = {
-  name: "Ha",
-  age: 22,
-};
-let keys = Object.keys(person); // trả về key
-let keys2 = Object.values(person); // trả về value
-let entries = Object.entries(person); // trả về cả key và value
-// assin sao chép src 1, 2 vàp target
-let target = { c: 100 };
-let src1 = { a: 0 };
-let src2 = { b: 50 };
-Object.assign(target, src1, src2);
-console.log(target);
-// kiểm tra có thuộc tính không
-let person2 = {
-  name: "Ha",
-  age: 22,
-};
-console.log(person2.hasOwnProperty("name")); // kiểm tra trong person2 có thuộc tính name không
-Object.freeze(person2); // đóng băng đối tượng không thể thêm  sửa xóa
-let person3 = {
-  name: "Ha",
-  age: 12,
-};
-Object.seal(person3); // chỉ sửa không thêm xóa được
+function SinhVien(fullName, ID, birthYear, homeTown) {
+  // thuộc tính
+  this.fullName = fullName;
+  this.ID = ID;
+  this.birthYear = birthYear;
+  this.homeTown = homeTown;
+  // phương thức
+  this.showInfo = function () {
+    return `${this.fullName} ${this.ID} ${this.homeTown}`;
+  };
+}
+const sv1 = new SinhVien("Nguyen Hong Ha", 1, 2003, "Thanh Hóa");
+const sv2 = new SinhVien("Nguyen Ha", 2, 2004, "HN");
+console.log(sv1.showInfo());
