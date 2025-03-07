c0a83db989d532e3eeb234bd693ee46a88476b49
NgHongHa <ha2k3.dev@gmail.com>
2025-03-07 12:17:29 +0700

    ví dụ về tính kế thừa trong class

--- Theory/main.js
+++ Theory/main.js
@@ -1,26 +1,39 @@
-// dinn nghia
-class School {
-  constructor(id, name, birthYear) {
-    this.id = id;
+// bt
+// class cha
+class Shape {
+  constructor(name) {
     this.name = name;
-    this.birthYear = birthYear;
   }
-  // phuong thuc
-  calcAge(currentYear) {
-    return currentYear - this.birthYear;
-  }
-}
-// tao doi tuong cua lop shool
-const p1 = new School("p2001", "Ha", 2003);
-console.log(p1.name);
-// them lop Student ke thua tu School
-
-class Student extends School {
-  constructor(id, name, birthYear, major) {
-    // ke thua id, name, birthYear, them major
-    super(id, name, birthYear);
-    this.major = major;
+  // phuong thu tinh dien tic, mac dinh return ve 0
+  calcArea() {
+    return 0;
   }
 }
-const s1 = new School("s2001", "HaStudent", 2003);
-console.log(s1.name);
+// lop con hinh vuong
+class Square extends Shape {
+  constructor(name, size) {
+    super(name);
+    this.size = size;
+  }
+  // ghi de phuong thuc tinh dien tich
+  calcArea() {
+    return this.size * this.size;
+  }
+}
+// lop con hcm
+class Rectangle extends Shape {
+  constructor(name, width, height) {
+    super(name);
+    this.width = width;
+    this.height = height;
+  }
+  // override phuong thuc tinh dien tich hcn
+  calcArea() {
+    return this.width * this.height;
+  }
+}
+// toa cac doi tuong hinh
+const sq1 = new Square("hinh vuong", 4);
+const rec1 = new Rectangle("hcn", 4, 6);
+console.log(sq1.calcArea());
+console.log(rec1.calcArea());
